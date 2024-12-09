f58dbdd59ab2bd8177768398f77a7dd050cbed10
chamath-ranasinghe <chamathmmw@gmail.com>
2024-12-09 13:08:26 +0530

    Tried out routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+];
